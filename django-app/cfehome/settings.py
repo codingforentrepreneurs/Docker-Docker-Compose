cf7afbd2ecda11ac6f02a59feb37bce1f8cd0699
CFE <hello@teamcfe.com>
2021-04-22 22:24:58 -0700

    20 - Django Static Files & Nginx

--- django-app/cfehome/settings.py
+++ django-app/cfehome/settings.py
@@ -118,8 +118,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' # http://localhost/static/
+STATIC_ROOT = BASE_DIR / "staticfiles" # collectstatic
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
+
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- django-app/run.sh
+++ django-app/run.sh
@@ -4,5 +4,7 @@
 
 /usr/local/bin/python manage.py createsuperuser --noinput
 
+/usr/local/bin/python manage.py collectstatic --noinput
+
 RUN_PORT=${PORT:-8000}
 /usr/local/bin/gunicorn cfehome.wsgi:application --bind "0.0.0.0:${RUN_PORT}"
